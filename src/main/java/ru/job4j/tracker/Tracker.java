7f929f02695d07f810a1a0802dbacd3ad7ca3a3d
Shaman2k <nzagorodnikov@gmail.com>
2024-01-19 17:02:03 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #474595]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -36,12 +36,11 @@ public class Tracker {
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
         if (index != -1) {
-            item.setId(items[index].getId());
+            item.setId(id);
             items[index] = item;
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
     private int indexOf(int id) {
