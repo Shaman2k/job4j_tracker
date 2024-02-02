f26394c5644d1a17b282212af41f794aa1772524
Shaman2k <nzagorodnikov@gmail.com>
2024-02-02 14:58:33 +0300

    Именование классов реализующих интерфейс [#505124]

--- src/main/java/ru/job4j/tracker/action/Replace.java
+++ src/main/java/ru/job4j/tracker/action/Replace.java
@@ -1,9 +1,11 @@
-package ru.job4j.tracker;
+package ru.job4j.tracker.action;
 
-public class ReplaceAction implements UserAction {
+import ru.job4j.tracker.*;
+
+public class Replace implements UserAction {
     private final Output output;
 
-    public ReplaceAction(Output output) {
+    public Replace(Output output) {
         this.output = output;
     }
 
