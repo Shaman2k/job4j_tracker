4fde4a814a34770d1ec1ebe5bb092f3a74302acc
Shaman2k <nzagorodnikov@gmail.com>
2024-03-11 10:40:49 +0300

    1. Организовать сортировку User [#10034 #485396]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -16,7 +16,7 @@ public class User implements Comparable<User> {
     public int compareTo(User o) {
         int result = this.name.compareTo(o.name);
         if (result == 0) {
-            result = this.age - o.age;
+            result = Integer.compare(this.age, o.age);
         }
         return result;
     }
