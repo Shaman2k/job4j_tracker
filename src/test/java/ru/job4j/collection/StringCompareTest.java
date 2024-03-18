b474bc20fe2cd6da56f55b00bd4167f6286af886
Shaman2k <nzagorodnikov@gmail.com>
2024-03-18 12:53:49 +0300

    3. Компаратор для строк [#35008]

---
+++ src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCompareTest {
+    @Test
+    public void whenStringsAreEqualThenZero() {
+        StringCompare compare = new StringCompare();
+        int result = compare.compare(
+                "Ivanov",
+                "Ivanov"
+        );
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void whenLeftLessThenNegative() {
+        StringCompare compare = new StringCompare();
+        int result = compare.compare(
+                "Ivanov",
+                "Ivanova"
+        );
+        assertThat(result).isLessThan(0);
+    }
+
+    @Test
+    public void whenLeftGreaterThenPositive() {
+        StringCompare compare = new StringCompare();
+        int result = compare.compare(
+                "Petrov",
+                "Ivanova"
+        );
+        assertThat(result).isGreaterThan(0);
+    }
+
+    @Test
+    public void secondCharOfLeftGreaterThenPositive() {
+        StringCompare compare = new StringCompare();
+        int result = compare.compare(
+                "Petrov",
+                "Patrov"
+        );
+        assertThat(result).isGreaterThan(0);
+    }
+
+    @Test
+    public void secondCharOfLeftLessThenNegative() {
+        StringCompare compare = new StringCompare();
+        int result = compare.compare(
+                "Patrova",
+                "Petrov"
+        );
+        assertThat(result).isLessThan(0);
+    }
+}
