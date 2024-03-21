1c40da9faafb1b458887e1c3da26b5a2d4f842b1
Shaman2k <nzagorodnikov@gmail.com>
2024-03-21 11:33:05 +0300

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -0,0 +1,11 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepartmentsDescComparator implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int result = right.split("/")[0].compareTo(left.split("/")[0]);
+        return result == 0 ? left.compareTo(right) : result;
+    }
+}
